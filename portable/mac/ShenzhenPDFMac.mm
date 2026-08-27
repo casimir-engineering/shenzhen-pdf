db6be420d0713e0c040ee61c50cfba368a91ef77
Raphaël Casimir <mail@raph.io>
2026-08-28 04:47:18 +0800

    Release 26.8.28 (build 1): Paginated Markdown and August reader improvements

--- portable/mac/ShenzhenPDFMac.mm
+++ portable/mac/ShenzhenPDFMac.mm
@@ -2362,7 +2362,7 @@ static void spdf_discard_launch_prerender(void) {
     NSDictionary* info = NSBundle.mainBundle.infoDictionary;
     NSString* version = info[@"CFBundleShortVersionString"];
     NSString* build = info[(NSString*)kCFBundleVersionKey];
-    if (version.length == 0) version = @"26.8.27";
+    if (version.length == 0) version = @"26.8.28";
     if (build.length == 0) build = @"1";
     return [NSString stringWithFormat:@"%@-%@", version, build];
 }
