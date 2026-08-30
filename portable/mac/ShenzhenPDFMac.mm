4edf659f7736f463050b9e89926793fa0383f26b
Raphaël Casimir <mail@raph.io>
2026-08-31 00:34:08 +0800

    Release 26.8.31 (build 1): Shenzhen Files as the automatic file manager, plus an updater download-bound fix

--- portable/mac/ShenzhenPDFMac.mm
+++ portable/mac/ShenzhenPDFMac.mm
@@ -2443,7 +2443,7 @@ static id spdf_state_object_from_yaml_data(NSData* data) {
     NSDictionary* info = NSBundle.mainBundle.infoDictionary;
     NSString* version = info[@"CFBundleShortVersionString"];
     NSString* build = info[(NSString*)kCFBundleVersionKey];
-    if (version.length == 0) version = @"26.8.30";
+    if (version.length == 0) version = @"26.8.31";
     if (build.length == 0) build = @"1";
     return [NSString stringWithFormat:@"%@-%@", version, build];
 }
