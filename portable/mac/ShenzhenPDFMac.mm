8cbdeda9695810919e829075f6f9e8c98063246c
Raphaël Casimir <mail@raph.io>
2026-09-03 18:22:04 +0800

    Release 26.9.3 (build 1): Per-document Keep Image Colors, dark-theme page edges, diagram and minimap fixes

--- portable/mac/ShenzhenPDFMac.mm
+++ portable/mac/ShenzhenPDFMac.mm
@@ -2177,7 +2177,7 @@ id spdf_state_object_from_yaml_data(NSData* data) {
     NSDictionary* info = NSBundle.mainBundle.infoDictionary;
     NSString* version = info[@"CFBundleShortVersionString"];
     NSString* build = info[(NSString*)kCFBundleVersionKey];
-    if (version.length == 0) version = @"26.9.2";
+    if (version.length == 0) version = @"26.9.3";
     if (build.length == 0) build = @"1";
     return [NSString stringWithFormat:@"%@-%@", version, build];
 }
